89c3381868dd61c6abafc0c985745d9d004fa778
Ewan Nicolson <ewan.nicolson@gmail.com>
2022-07-21 18:49:31 +0100

    linking to existing notes is possible

--- lua/nvim-zettel/init.lua
+++ lua/nvim-zettel/init.lua
@@ -1,3 +1,12 @@
+local has_telescope, telescope = pcall(require, "telescope")
+
+if not has_telescope then
+    error(
+        "This requires telescope.nvim"
+    )
+end
+
+
 local function GetZettelDir()
     return os.getenv("ZETTELKASTENDIR")
 end
@@ -24,14 +33,48 @@ local function NewZettel(noteName)
 end
 
 local function RelatedZettel(noteName)
-    local pos = vim.api.nvim_win_get_cursor(0)
     local notePath = NoteLocalPath(noteName)
     local newlabel = "[" .. noteName .. "](./" .. notePath .. ")"
-    vim.api.nvim_buf_set_lines(0, pos[1], pos[1], false, {newlabel})
+    vim.api.nvim_paste(newlabel, true, -1)
     vim.api.nvim_command("e " .. notePath)
+end
+
+
+local function getLocalPath(path)
+    return path:match("[^/]*.$")
+end
+
+local function localPathToName(localPath)
+    local withoutdate, _ = string.gsub(localPath, "^%d+-", "", 1)
+    local withoutmd, _ = string.gsub(withoutdate, "%.md$", "", 1)
+    return withoutmd
+end
+
+local function PasteLink()
+    -- took a lot from here: https://gitlab.com/thlamb/telescope-zettel.nvim/-/blob/main/lua/telescope/_extensions/zettel.lua
+    return function(prompt_bufnr)
+        local entry = require("telescope.actions.state").get_selected_entry()
+        require("telescope.actions").close(prompt_bufnr)
+        local entryPath = entry.path:gsub("%/./", "/") -- Clean path in grep mode
+        local localPath = getLocalPath(entryPath)
+        local noteName = localPathToName(localPath)
+        local link = "[" .. noteName .. "](./" .. localPath .. ")"
+        vim.api.nvim_paste(link, true, -1)
+    end
+end
+
+local function LinkExistingZettel(opts)
+    opts = opts or {}
+    opts.attach_mappings = function(_, map)
+        map("i", "<cr>", PasteLink())
+        map("n", "<cr>", PasteLink())
+        return true
+    end
+    require("telescope.builtin").live_grep(opts)
 end
 
 return {
     NewZettel = NewZettel,
-    RelatedZettel = RelatedZettel
+    RelatedZettel = RelatedZettel,
+    LinkExistingZettel = LinkExistingZettel,
 }
--- plugin/nvim-zettel.vim
+++ plugin/nvim-zettel.vim
@@ -5,3 +5,5 @@ let g:loaded_nvimzettel = 1
 
 command! -nargs=1 NewZettel lua require"nvim-zettel".NewZettel(<f-args>)
 command! -nargs=1 RelatedZettel lua require"nvim-zettel".RelatedZettel(<f-args>)
+command! LinkExistingZettel lua require"nvim-zettel".LinkExistingZettel()
+
